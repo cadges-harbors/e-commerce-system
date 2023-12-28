5717063890378c2370125aa1ff598e5eb005b1b9
Anelina C <152532976+cadges-harbors@users.noreply.github.com>
2023-12-28 18:29:35 +0600

    Create Customer.php

---
+++ Customer.php
@@ -0,0 +1,19 @@
+<?php
+
+class Customer {
+    private $customerId;
+    private $name;
+    private $email;
+
+    public function __construct($customerId, $name, $email) {
+        $this->customerId = $customerId;
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function displayInfo() {
+        echo "Customer ID: $this->customerId\n";
+        echo "Name: $this->name\n";
+        echo "Email: $this->email\n";
+    }
+}
